64a606eea060798e1f72d445c641954f657794db
Chris Mascioli <chrism315@gmail.com>
2018-11-07 15:15:08 -0600

    Create sampling.py

---
+++ algos/sampling.py
@@ -0,0 +1,21 @@
+import random
+import math
+
+def truncation_selection(population, n):
+    population.sort(key=lambda x: x.fitness, reverse=True)
+    return population[:n]
+
+def tournament_selection(population, n, p):
+    winners = []
+    for i in range(n):
+        if p%1 != 0:
+            if random.random() < p%1:
+                loop_p = math.ceil(p)
+            else:
+                loop_p = math.floor(p)
+        else:
+            loop_p = p
+        contestants = [random.choice(population) for x in range(loop_p)]
+        contestants.sort(key=lambda x: x.fitness, reverse=True)
+        winners.append(contestants[0])
+    return winners
